00bccdb66db883e6c0f2569ed5a400e73ce9e6f4
Aditya Rajput <adiraj20072002@gmail.com>
2026-01-22 23:27:11 +0530

    Automate seeding demo data

--- app/src/main/java/co/adityarajput/alarmetrics/data/AppContainer.kt
+++ app/src/main/java/co/adityarajput/alarmetrics/data/AppContainer.kt
@@ -6,6 +6,7 @@ import co.adityarajput.alarmetrics.data.alarm.AlarmsRepository
 import co.adityarajput.alarmetrics.data.record.Record
 import co.adityarajput.alarmetrics.data.record.RecordsRepository
 import co.adityarajput.alarmetrics.enums.AlarmApp
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.runBlocking
 import kotlin.random.Random
 
@@ -19,28 +20,30 @@ class AppContainer(private val context: Context) {
 
     fun seedDemoData() {
         runBlocking {
-            val alarmId1 = alarmsRepository.save(Alarm("Wake up", AlarmApp.GOOGLE_CLOCK))
-            val alarmId2 = alarmsRepository.save(Alarm("Exercise", AlarmApp.SAMSUNG_REMINDER))
-            alarmsRepository.save(Alarm("Book club", AlarmApp.GOOGLE_CALENDAR))
-            for (i in 0..30) {
-                recordsRepository.create(
-                    Record(
-                        alarmId1,
-                        System.currentTimeMillis() - i * 86400000,
-                        Random.nextInt(2, 7),
-                    ),
-                )
-                recordsRepository.create(
-                    Record(
-                        alarmId2,
-                        System.currentTimeMillis() - i * 86400000,
-                        Random.nextInt(2, 15),
-                    ),
-                )
-            }
+            if (alarmsRepository.list().first().isEmpty()) {
+                val alarmId1 = alarmsRepository.save(Alarm("Wake up", AlarmApp.GOOGLE_CLOCK))
+                val alarmId2 = alarmsRepository.save(Alarm("Exercise", AlarmApp.SAMSUNG_REMINDER))
+                alarmsRepository.save(Alarm("Book club", AlarmApp.GOOGLE_CALENDAR))
+                for (i in 0..30) {
+                    recordsRepository.create(
+                        Record(
+                            alarmId1,
+                            System.currentTimeMillis() - i * 86400000,
+                            Random.nextInt(2, 7),
+                        ),
+                    )
+                    recordsRepository.create(
+                        Record(
+                            alarmId2,
+                            System.currentTimeMillis() - i * 86400000,
+                            Random.nextInt(2, 15),
+                        ),
+                    )
+                }
 
-            alarmsRepository.save(Alarm("Sleep", AlarmApp.GOOGLE_CLOCK, false))
-            alarmsRepository.save(Alarm("Catch up with Neil", AlarmApp.SAMSUNG_CALENDAR, false))
+                alarmsRepository.save(Alarm("Sleep", AlarmApp.GOOGLE_CLOCK, false))
+                alarmsRepository.save(Alarm("Catch up with Neil", AlarmApp.SAMSUNG_CALENDAR, false))
+            }
         }
     }
 }
