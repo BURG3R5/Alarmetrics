c8b97badfdb8933c958c9e195db1d2a0f2b8721d
Aditya Rajput <adiraj20072002@gmail.com>
2026-02-05 20:27:07 +0530

    Update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "co.adityarajput.alarmetrics"
         minSdk = 29
         targetSdk = 36
-        versionCode = 5
-        versionName = "2.1.0"
+        versionCode = 6
+        versionName = "2.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
