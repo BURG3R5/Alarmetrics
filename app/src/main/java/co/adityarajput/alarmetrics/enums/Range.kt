d2e2820f089a7b86042839417a51f37d024c3210
Aditya Rajput <adiraj20072002@gmail.com>
2026-02-25 16:51:40 +0530

    Use global average instead of previous range's average as target
    Sorry I keep changing what the target line means, but it just didn't feel like an accurate metric before.

--- app/src/main/java/co/adityarajput/alarmetrics/enums/Range.kt
+++ app/src/main/java/co/adityarajput/alarmetrics/enums/Range.kt
@@ -2,9 +2,9 @@ package co.adityarajput.alarmetrics.enums
 
 import co.adityarajput.alarmetrics.R
 
-enum class Range(val displayName: Int) {
-    WEEK(R.string.week),
-    MONTH(R.string.month),
-    YEAR(R.string.year),
-    DECADE(R.string.decade),
+enum class Range(val displayName: Int, val numberOfDaysInChild: Float) {
+    WEEK(R.string.week, 1F),
+    MONTH(R.string.month, 7F),
+    YEAR(R.string.year, 30.4375F),
+    DECADE(R.string.decade, 365.25F),
 }
--- app/src/main/java/co/adityarajput/alarmetrics/viewmodels/AlarmsViewModel.kt
+++ app/src/main/java/co/adityarajput/alarmetrics/viewmodels/AlarmsViewModel.kt
@@ -13,18 +13,16 @@ import co.adityarajput.alarmetrics.enums.DialogState
 import co.adityarajput.alarmetrics.enums.Range
 import co.adityarajput.alarmetrics.utils.Logger
 import co.adityarajput.alarmetrics.utils.getEndOfRange
-import co.adityarajput.alarmetrics.utils.getStartOfRange
 import co.adityarajput.alarmetrics.utils.indexIn
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
-import kotlin.math.max
 import kotlin.math.round
 
 data class AlarmsState(val state: List<AlarmWithStats>? = null)
 data class ChartState(
-    val snoozeTimes: List<Int>? = null,
+    val snoozeTimes: List<Float>? = null,
     val unit: Int? = null,
-    val previousRangeAverage: Int? = null,
+    val averageSnoozeTime: Float? = null,
 )
 
 class AlarmsViewModel(private val repository: Repository) : ViewModel() {
@@ -43,51 +41,36 @@ class AlarmsViewModel(private val repository: Repository) : ViewModel() {
         val flow = MutableStateFlow(ChartState())
 
         val to = from.getEndOfRange(range)
-        val previousTo = from - 1
-        val previousFrom = previousTo.getStartOfRange(range)
 
         viewModelScope.launch {
-            repository.records(alarm.id, from, to)
-                .combine(repository.records(alarm.id, previousFrom, previousTo)) { s, t -> s to t }
-                .collect { value ->
-                    val records = value.first
-                    val snoozeTimes = MutableList(to.indexIn(range) + 1) { 0L }
-                    records.forEach { snoozeTimes[it.firstSnooze.indexIn(range)] += it.snoozeTime }
-                    val max = snoozeTimes.max()
+            repository.records(alarm.id, from, to).combine(alarms, ::Pair).collect { value ->
+                val records = value.first
+                val snoozeTimes = MutableList(to.indexIn(range) + 1) { 0L }
+                records.forEach { snoozeTimes[it.firstSnooze.indexIn(range)] += it.snoozeTime }
 
-                    val previousRangeRecords = value.second
-                    val previousRangeSnoozeTimes = MutableList(previousTo.indexIn(range) + 1) { 0L }
-                    previousRangeRecords.forEach {
-                        previousRangeSnoozeTimes[it.firstSnooze.indexIn(range)] += it.snoozeTime
-                    }
-                    val average = previousRangeSnoozeTimes.sum() /
-                            max(previousRangeSnoozeTimes.count { it > 0L }, 1)
+                val max = snoozeTimes.max()
+                val average =
+                    value.second.state!!.first { it.alarm == alarm }.averageSnoozeTime * range.numberOfDaysInChild
 
-                    var unit = R.string.days
-                    var divisor = 86_400_000f
+                var unit = R.string.days
+                var divisor = 86_400_000f
 
-                    if (max < 10 * 60_000L) {
-                        divisor = 1000f
-                        unit = R.string.seconds
-                    } else if (max < 10 * 3_600_000L) {
-                        divisor = 60_000f
-                        unit = R.string.minutes
-                    } else if (max < 10 * 86_400_000L) {
-                        divisor = 3_600_000f
-                        unit = R.string.hours
-                    }
-
-                    var target: Int? = round(average / divisor).toInt()
-                    // INFO: Can't display target line if it's higher than max bar value
-                    if (target == 0 || average > max || range == Range.DECADE) {
-                        target = null
-                    }
-
-                    flow.value = ChartState(
-                        snoozeTimes.map { round(it / divisor).toInt() },
-                        unit, target,
-                    )
+                if (max < 10 * 60_000L) {
+                    divisor = 1000f
+                    unit = R.string.seconds
+                } else if (max < 10 * 3_600_000L) {
+                    divisor = 60_000f
+                    unit = R.string.minutes
+                } else if (max < 10 * 86_400_000L) {
+                    divisor = 3_600_000f
+                    unit = R.string.hours
                 }
+
+                flow.value = ChartState(
+                    snoozeTimes.map { round(it / divisor) }, unit,
+                    round(average / divisor).takeUnless { it == 0F || average > max },
+                )
+            }
         }
 
         return flow
